371777d42b4ec9d4312eadec0ce9f189d974be04
Mario Ascaso <ascasovicentemario@gmail.com>
2026-01-17 23:10:33 +0100

    addingPiecesToThePuzzle

---
+++ backForms/src/main/java/com/daw/backForms/BackFormsApplication.java
@@ -0,0 +1,13 @@
+package com.daw.backForms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackFormsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackFormsApplication.class, args);
+	}
+
+}
